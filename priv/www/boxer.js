e2ba210293a12a93b930c86c786e9219ca40770f
Bikram Chatterjee <razorpeak@gmail.com>
2012-08-07 21:17:37 +0200

    Query display and editing

--- priv/www/boxer.js
+++ priv/www/boxer.js
@@ -1,41 +1,4 @@
 var iniDisplay = "inline";
-var treeroot =
-           { name : "select",
-             top : 0,
-             height : 0,
-             children: [ { name : "",
-                           display: iniDisplay,
-                           children: [
-                               { name : "atom", display: iniDisplay, children: []}
-                               , { name : "big", display: iniDisplay, children: []}
-                               , { name : "cool", display: iniDisplay, children: []}]
-                         },
-                         { name : "from",
-                           display: iniDisplay,
-                           children: [
-                               { name : "apple", display: iniDisplay, children: []}
-                               , { name : "ball", display: iniDisplay, children: []}
-                               , { name : "cat", display: iniDisplay, children: []}]
-                         },
-                         { name : "where",
-                           display: iniDisplay,
-                           children: [
-                               { name : "",
-                                 display: iniDisplay,
-                                 children: [
-                                     { name : "man", display: iniDisplay, children: []},
-                                     { name : "=", display: iniDisplay, children: []},
-                                     { name : "monkey", display: iniDisplay, children: []}]
-                               },
-                               { name : "and",
-                                 display: iniDisplay,
-                                 children: [
-                                     { name : "woman", display: iniDisplay, children: []},
-                                     { name : "=", display: iniDisplay, children: []},
-                                     { name : "ape", display: iniDisplay, children: []}]
-                               }]
-                         }]
-           };
 
 function loadsqlfiles()
 {    
@@ -47,7 +10,7 @@ function loadsqlfiles()
     
     var fileref=document.createElement('script');
     fileref.setAttribute("type","text/javascript");
-    fileref.setAttribute("src", "sqls.js?" + Math.floor((Math.random()*1000)+1));
+    fileref.setAttribute("src", "sql/sqls.js?" + Math.floor((Math.random()*1000)+1));
     fileref.setAttribute("id", "sqlscriptfiles");
     fileref.onload = function() {
         var qfilesList = document.getElementById("qfiles");
@@ -75,11 +38,13 @@ function loadsql()
     
     var fileref=document.createElement('script');
     fileref.setAttribute("type","text/javascript");
-    fileref.setAttribute("src", file + "?" + Math.floor((Math.random()*1000)+1));
+    fileref.setAttribute("src", "sql/" + file + "?" + Math.floor((Math.random()*1000)+1));
     fileref.setAttribute("id", "sqlscriptfiles");
     fileref.onload = function() {
         document.getElementById("sqltext").innerHTML = parsetree.sql();
-        reload_tree(parsetree.json());
+        var tree = parsetree.json();
+        prep_tree(tree);
+        build_boxes(tree);
     }
     document.getElementsByTagName("head")[0].appendChild(fileref);
 }
@@ -87,6 +52,7 @@ function loadsql()
 function build_boxes(tree)
 {
     $('#container').text('');
+    $('#container').data('treeroot', tree);
     build_boxes_r(tree, $('#container'), 1, 0);
 }
 
@@ -97,6 +63,7 @@ function prep_tree(tree)
     for (var i=0; i < tree.children.length ; ++i) {
         tree.children[i]["top"] = 0;
         tree.children[i]["height"] = 0;
+        tree.children[i]["summary"] = get_text(tree.children[i], '');
         if(i == 0 ) {
             if(tree.name.length > 0) tree.children[i].top = def_height;
         } else
@@ -106,36 +73,58 @@ function prep_tree(tree)
     }
 }
 
+function get_text(tree, txt)
+{
+    txt = (tree.name + ' ');
+    for(var i=0; i < tree.children.length; ++i) {
+        txt += get_text(tree.children[i], txt) + ' ';
+    }
+    return txt.trim();
+}
+
 var def_height = 20;
 var tab_len    = 10;
 function build_boxes_r(tree, parent_node, left, depth) {
-    var col = (200 - 20 * depth);
+    var bgcol = (255 - 10 * depth);
+    //var col = (30 * depth);
     var node = (tree.name.length > 0
-                ? $('<div id="'+tree.name+'"></div>').text(tree.name)
+                ? $('<div id="'+tree.name+'"></div>').append('<span>'+tree.name+'</span>')
                 : $('<div></div>'));
+
+    var editbx = $('<textarea class="edit_div"></textarea>')
+        .hide()
+        .text(tree.summary)
+        .appendTo(node);
 
     node.dblclick(function(evt) {
             event.stopPropagation();
             var dom = $(this).data("tree");
+            var edit = $(this).data("edit");
             for(var i=0; i < dom.children.length; ++i)
                 if(dom.children[i].display == "none")
                     dom.children[i].display = iniDisplay;
                 else
                     dom.children[i].display = "none";
-            build_boxes();
+            build_boxes($('#container').data('treeroot'));
         })
         .addClass('inner_div')
         .css('display', tree.display)
-        .css('background-color', 'rgb('+col+','+col+','+col+')')
-        .data("tree", tree);
-    node.css('top', tree.top)
+        .css('background-color', 'rgb('+bgcol+','+bgcol+','+bgcol+')')
+        //.css('color', 'rgb('+col+','+col+','+col+')')
+        .data("tree", tree)
+        .data("edit", editbx)
+        .css('top', tree.top)
         .css('left', left + tab_len);
+
     parent_node.append(node);
+    if(tree.display == "none" && parent_node.data("edit") != undefined)
+        parent_node.data("edit").show();
 
     for (var i=0; i < tree.children.length ; ++i) {
         build_boxes_r(tree.children[i], node, left, depth + 1);
     }
     node.height(tree.height);
+    editbx.height(tree.height - def_height);
 }
 
 function get_node_bottom(n)
@@ -156,6 +145,43 @@ function get_node_bottom(n)
 
 $(document).ready(function()
 {
-    prep_tree(treeroot);
-    build_boxes(treeroot);
+    loadsqlfiles();
 });
+
+//var treeroot =
+//           { name : "select",
+//             top : 0,
+//             height : 0,
+//             children: [ { name : "",
+//                           display: iniDisplay,
+//                           children: [
+//                               { name : "atom", display: iniDisplay, children: []}
+//                               , { name : "big", display: iniDisplay, children: []}
+//                               , { name : "cool", display: iniDisplay, children: []}]
+//                         },
+//                         { name : "from",
+//                           display: iniDisplay,
+//                           children: [
+//                               { name : "apple", display: iniDisplay, children: []}
+//                               , { name : "ball", display: iniDisplay, children: []}
+//                               , { name : "cat", display: iniDisplay, children: []}]
+//                         },
+//                         { name : "where",
+//                           display: iniDisplay,
+//                           children: [
+//                               { name : "",
+//                                 display: iniDisplay,
+//                                 children: [
+//                                     { name : "man", display: iniDisplay, children: []},
+//                                     { name : "=", display: iniDisplay, children: []},
+//                                     { name : "monkey", display: iniDisplay, children: []}]
+//                               },
+//                               { name : "and",
+//                                 display: iniDisplay,
+//                                 children: [
+//                                     { name : "woman", display: iniDisplay, children: []},
+//                                     { name : "=", display: iniDisplay, children: []},
+//                                     { name : "ape", display: iniDisplay, children: []}]
+//                               }]
+//                         }]
+//           };
