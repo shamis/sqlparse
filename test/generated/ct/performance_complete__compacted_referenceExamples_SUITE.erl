e270032df00df5b90b06a5adc330a499c1069077
Walter Weinmann <walter-weinmann@users.noreply.github.com>
2018-01-08 16:15:33 +0100

    Add SQL formatter functionality to sqlparse_fold. (#114)
    * Add SQL formatter functionality to sqlparse_fold.
    
    * Add SQL formatter functionality to sqlparse_fold.
    
    * Add SQL formatter functionality to sqlparse_fold.
    
    * Add SQL formatter functionality to sqlparse_fold.
    
    * Add SQL formatter functionality to sqlparse_fold.

--- test/generated/ct/performance_complete__compacted_referenceExamples_SUITE.erl
+++ test/generated/ct/performance_complete__compacted_referenceExamples_SUITE.erl
@@ -2,7 +2,7 @@
 %%% File        : performance_complete__compacted_referenceExamples_SUITE.erl
 %%% Description : Test Suite for rule: referenceExamples.
 %%%
-%%% Created     : 08.12.2017
+%%% Created     : 05.01.2018
 %%%-------------------------------------------------------------------
 -module(performance_complete__compacted_referenceExamples_SUITE).
 
--- test/generated/ct/performance_complete__compacted_special_SUITE.erl
+++ test/generated/ct/performance_complete__compacted_special_SUITE.erl
@@ -2,7 +2,7 @@
 %%% File        : performance_complete__compacted_special_SUITE.erl
 %%% Description : Test Suite for rule: special.
 %%%
-%%% Created     : 08.12.2017
+%%% Created     : 05.01.2018
 %%%-------------------------------------------------------------------
 -module(performance_complete__compacted_special_SUITE).
 
